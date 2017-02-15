bc5a7ee381cc8102bf8e8477532aa3dcaf144156
Simon Humbert <shumbert@kernix.com>
2017-02-15 12:02:37 +0100

    test cron + update template email

--- src/AppBundle/Command/ContestCommand.php
+++ src/AppBundle/Command/ContestCommand.php
@@ -27,33 +27,33 @@ class ContestCommand extends ContainerAwareCommand
         $fb = $this->getContainer()->get('facebook_service');
         $app = $fb->getApp();
         $admins = $fb->getAdmins();
-        $contest = $doctrine->getRepository('AppBundle:Contest')->findOneBy(['status' => 1]);
-//        if ($contest->getDateEnd()->format('Y-m-d') == date('Y-m-d')) {
-            $winner = $doctrine->getRepository('AppBundle:ContestParticipant')->getContestWinner($contest);
+//        $contest = $doctrine->getRepository('AppBundle:Contest')->findOneBy(['status' => 1]);
+////        if ($contest->getDateEnd()->format('Y-m-d') == date('Y-m-d')) {
+//            $winner = $doctrine->getRepository('AppBundle:ContestParticipant')->getContestWinner($contest);
+//
+//            $winner = $doctrine->getRepository('AppBundle:ContestParticipant')->find($winner[0]->getId());
+//            $contest->setWinner($winner);
+//            $em->flush();
+//            $photo = $winner->getPhoto();
+//        $fbApp = new FacebookApp($this->getContainer()->getParameter('appId'), $this->getContainer()->getParameter('appSecret'));
+//            foreach ($contest->getContestParticipants() as $contestParticipant) {
+//                $album_details = array(
+//                    'message'=> 'Le concours auquel j\'ai participé est terminé, voici la photo du gagnant !',
+//                    'link' => $this->getContainer()->get('router')->generate('photo_display', array('facebookId' =>$photo->getFacebookId()), 0),
+//                    'object_attachment' => $photo->getFacebookId(),
+//                    'access_token' => $app->getApp()->getAccessToken()->getValue()
+//                );
+////                $post_message = $app->post('/'.$contestParticipant->getParticipant()->getUser()->getFacebookId().'/feed', $album_details);
+//                try{
+//
+//                }catch (\Exception $e) {
+//                    var_dump($e);
+//                }
+//                $post_message = $app->post('/117131608805366/feed', $album_details);
+//
+//            }
 
-            $winner = $doctrine->getRepository('AppBundle:ContestParticipant')->find($winner[0]->getId());
-            $contest->setWinner($winner);
-            $em->flush();
-            $photo = $winner->getPhoto();
-        $fbApp = new FacebookApp($this->getContainer()->getParameter('appId'), $this->getContainer()->getParameter('appSecret'));
-            foreach ($contest->getContestParticipants() as $contestParticipant) {
-                $album_details = array(
-                    'message'=> 'Le concours auquel j\'ai participé est terminé, voici la photo du gagnant !',
-                    'link' => $this->getContainer()->get('router')->generate('photo_display', array('facebookId' =>$photo->getFacebookId()), 0),
-                    'object_attachment' => $photo->getFacebookId(),
-                    'access_token' => $app->getApp()->getAccessToken()->getValue()
-                );
-//                $post_message = $app->post('/'.$contestParticipant->getParticipant()->getUser()->getFacebookId().'/feed', $album_details);
-                try{
-
-                }catch (\Exception $e) {
-                    var_dump($e);
-                }
-                $post_message = $app->post('/117131608805366/feed', $album_details);
-
-            }
-
-//            $this->sendMail($admins);
+            $this->sendMail($admins);
 //        }
 
     }
